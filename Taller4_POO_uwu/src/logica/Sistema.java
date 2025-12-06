28def893320a3141c9f82e3b69e10a08a28453c0
cxrtees <benjamincortes2889@gmail.com>
2025-12-06 15:42:48 -0300

    creacion Sistema.java

---
+++ Taller4_POO_uwu/src/logica/Sistema.java
@@ -0,0 +1,43 @@
+package logica;
+
+import java.util.ArrayList;
+
+import dominio.AsignaturaCertificacion;
+import dominio.Certificacion;
+import dominio.Curso;
+import dominio.Estudiante;
+import dominio.Nota;
+import dominio.RegistroCertificacion;
+import dominio.Usuario;
+
+public class Sistema {
+	
+	private ArrayList<Usuario> usuarios = new ArrayList<>();
+	private ArrayList<Estudiante> estudiantes = new ArrayList<>();
+	private ArrayList<Curso> cursos = new ArrayList<>();
+	private ArrayList<Certificacion> certificaciones = new ArrayList<>();
+	private ArrayList<RegistroCertificacion> registros = new ArrayList<>();
+	private ArrayList<Nota> notas = new ArrayList<>();
+	private ArrayList<AsignaturaCertificacion> asignaturasCertificaciones = new ArrayList<>();
+	
+	public Sistema() {}
+
+	public ArrayList<Usuario> getUsuarios() {return usuarios;}
+
+	public ArrayList<Estudiante> getEstudiantes() {return estudiantes;}
+
+	public ArrayList<Curso> getCursos() {return cursos;}
+
+	public ArrayList<Certificacion> getCertificaciones() {return certificaciones;}
+
+	public ArrayList<RegistroCertificacion> getRegistros() {return registros;}
+
+	public ArrayList<Nota> getNotas() {return notas;}
+
+	public ArrayList<AsignaturaCertificacion> getAsignaturasCertificaciones() {return asignaturasCertificaciones;}
+	
+	
+	
+	
+	
+}
