cc4628233a4523a96a310a98a9db945e00df4696
Catalina Rojas <cataa.rojas100@gmail.com>
2025-12-09 18:05:29 -0300

    Create App.java

---
+++ Taller4_POO_uwu/src/dominio/App.java
@@ -0,0 +1,17 @@
+package dominio;
+
+import GUI.Menu;
+import dominio.Sistema;
+import logica.SistemaIn;
+
+public class App {
+
+    public static void main(String[] args) {
+        // Creamos el sistema (lógica/dominio)
+        SistemaIn sistema = new Sistema();
+
+        // Creamos el menú (GUI) y lo iniciamos
+        Menu menu = new Menu(sistema);
+        menu.iniciar();
+    }
+}
